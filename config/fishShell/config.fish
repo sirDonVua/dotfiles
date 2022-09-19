f98d8cdc3cbffdf7c97abd5528c7f272ac7b0fce
Mhammed Magdi <sirdonvua@pop-os.localdomain>
2022-09-19 13:12:49 +0300

    updated config.fish & and mv * to config

--- config/fishShell/config.fish
+++ config/fishShell/config.fish
@@ -1,4 +1,9 @@
-#alias sudo='doas'
+#Export#
+set -x EDITOR "vim"
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+#End#
+
+##alias##
 alias apt='sudo nala'
 alias pkgup='sudo nala upgrade'
 alias fishrc="vim ~/.config/fish/config.fish"
@@ -7,6 +12,10 @@ alias ls='lsd'
 alias lt='lsd --tree'
 alias cat='bat'
 
+##End##
+
+###commands run When luanch###
 figlet fish -c | lolcat
 #colorscript -r
 #starship init fish | source
+###END###
