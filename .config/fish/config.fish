ac52efe6bf2acc189be7740f21ed93df9328af56
sirdonvua <sirdonvua@noemail.com>
2022-10-17 12:32:16 +0300

    rewrite my config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,22 +1,52 @@
-#Export#
-set -U fish_greeting
-set -x EDITOR "vim"
+#                  __ _          __ _     _
+#  ___ ___  _ __  / _(_) __ _   / _(_)___| |__
+# / __/ _ \| '_ \| |_| |/ _` | | |_| / __| '_ \
+#| (_| (_) | | | |  _| | (_| |_|  _| \__ \ | | |
+# \___\___/|_| |_|_| |_|\__, (_)_| |_|___/_| |_|
+#                       |___/
+
+
+#--------------------------------
+#   exporting
+#--------------------------------
+# PATH
+set -e fish_user_paths
+set -U fish_user_paths $HOME/.local/bin $HOME/Applications $fish_user_paths
+# fish
+set fish_greeting            # fish greeting
+set TERM "xterm-256color"    # prober colors
+# text Editors
+set EDITOR "vim"
+set VISUAL "geany"
+# MANPAGER
 set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
-#End#
+#--------------------------------
 
-##alias##
-alias apt='sudo nala'
-alias pkgup='sudo nala upgrade'
-alias fishrc="vim ~/.config/fish/config.fish"
-alias deb='deb-get'
-alias ls='lsd'
-alias lt='lsd --tree'
-alias cat='bat'
+#--------------------------------
+#   aliasis
+#--------------------------------
+# => pakage managers
+# apt
+alias apt     "sudo nala"
+alias aptup   "sudo nala upgrade"
+# dnf
+alias dnf     "sudo dnf"
+# pacman
+alias pacman  "sudo aura"
+alias wcpac   "aura -Q | wc -l"
 
-##End##
+# => ls,cat,grep
+alias ls      "lsd -lAh"
+alias cat     "bat"
+alias grep    "grep --color=auto"
 
-###commands run When luanch###
-figlet fish -c | lolcat
-#colorscript -r
-#starship init fish | source
-###END###
+# => files and dir
+alias cp      "cp -ir"
+alias rm      "rm -i"
+alias mv      "mv -i"
+alias rmdir   "rm -r"
+alias mkdir   "mkdir -pv"
+
+# => starship prompt
+starship init fish | source
+#--------------------------------
