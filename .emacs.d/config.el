6602d72f3dea484793364ce7a134fb4006da3354
sirdonvua <fake@email.com>
2022-11-16 13:58:58 +0300

    update config.el

--- .emacs.d/config.el
+++ .emacs.d/config.el
@@ -1,9 +1,9 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;(package-refresh-contents) ; reload the repo could take some time so i suggest using emacs clinet
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
+  (package-refresh-contents)
   (package-install 'use-package))
   (setq use-package-always-ensure t) ; always make sure that the packages are installed
 
@@ -33,22 +33,17 @@
        "b B"   '(ibuffer-list-buffers :which-key "Ibuffer list buffers")
        "b K"   '(kill-buffer :which-key "Kill buffer"))
 
-;; launching dired
 (nvmap :prefix "SPC"
     "d" '(dired-jump :which-key "Launch dired")
     "." '(find-file :which-key "find file")
+    "f r" '(counsel-recentf :which-key "find recent file")
+    "f s" '(counsel-swiper :which-key "Search in a file")
     "r" '((lambda() (interactive) (load-file "~/.emacs.d/init.el"))
     :which-key "reload emacs"))
 
-;; makeing h,l do what the supposed to do
-(evil-define-key 'normal dired-mode-map
-(kbd "h") 'dired-up-directory
-(kbd "l") 'dired-find-file
-(kbd "SPC") 'nil) ; making keybindings start with SPC work in dired
-
 (nvmap :prefix "SPC"
-    "a" '((lambda () (interactive) (set-input-method 'arabic )) :which-key "Switch to arabic language" )
-    "e" '((lambda() (interactive) (set-input-method 'Tox)) :which-key "Switch to english language" ))
+    "a" '((lambda () (interactive) (set-input-method 'arabic)) :which-key "Switch to the secound language" )
+    "e" '((lambda() (interactive) (set-input-method 'TeX)) :which-key "Switch to english language" ))
 
 (use-package dashboard
   :config
@@ -60,24 +55,24 @@
   ;; change title
   (setq dashboard-banner-logo-title "I Love Emacs Games :)")
   (setq dashboard-center-content t) ; make the dashboared centered
-(setq dashboard-items '((recents  . 5)
-                        (bookmarks . 5)
-                        (agenda . 5)))
-; make dasboard work with the emacs client
-(setq initial-buffer-choice (lambda () (get-buffer-create "*dashboard*"))))
+  (setq dashboard-items '((recents  . 10)
+                        (bookmarks . 5)))
+  ; make dasboard work with the emacs client
+  (setq initial-buffer-choice (lambda () (get-buffer-create "*dashboard*"))))
 
-(set-face-attribute 'default nil :font "JetBrainsMono Nerd Font 16" :weight 'medium)
+(set-face-attribute 'default nil :font "JetBrainsMono Nerd Font 14" :weight 'medium)
 (set-face-attribute 'variable-pitch nil :font "UbuntuMono Nerd Font 16" :weight 'medium)
-(set-face-attribute 'fixed-pitch nil :font "JetBrainsMono Nerd Font 16" :weight 'medium)
+(set-face-attribute 'fixed-pitch nil :font "JetBrainsMono Nerd Font 14" :weight 'medium)
 
-;; uncomment only if using emacs client
-;(add-to-list 'default-frame-alist '(font . "JetBrainsMono Nerd Font 14" ))
+;; if the buffer is a daemon it will fix the daemon fonts.
+(if (daemonp)
+(add-to-list 'default-frame-alist '(font . "JetBrainsMono Nerd Font 14" )))
 
 (set-fontset-font "fontset-default"
 		  'arabic
 		  (font-spec :family "Amiri" :size 24 ))
 
-;; make RTL work will in org
+;; make RTL work will in org mode
 (defun set-bidi-env ()
   "interactive"
   (setq bidi-paragraph-direction 'nil))
@@ -96,6 +91,10 @@
 	doom-theme-enable-italic t)
   (load-theme 'doom-one t)) ; load the doom one theme
 
+(use-package doom-modeline
+  :config
+  (doom-modeline-mode 1))
+
 (use-package beacon
 :config
 (beacon-mode 1))
@@ -104,17 +103,95 @@
 
 (setq comp-async-report-warnings-errors nil)
 
+(setq vc-handled-backends nil)
+
 (setq scroll-conservatively 101) ;; value greater than 100 gets rid of half page jumping
 (setq mouse-wheel-scroll-amount '(3 ((shift) . 3))) ;; how many lines at a time
 (setq mouse-wheel-progressive-speed t) ;; accelerate scrolling
 (setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
 
+;; counsel
+(use-package counsel
+:after ivy
+:config (counsel-mode))
+
+;; ivy
+(use-package ivy
+:config (ivy-mode)
+(setq ivy-initial-inputs-alist nil))
+
+;; ivy-rich
+(use-package ivy-rich
+  :after ivy
+  :custom
+  (ivy-virtual-abbreviate 'full
+   ivy-rich-switch-buffer-align-virtual-buffer t
+   ivy-rich-path-style 'abbrev)
+  :config
+  (ivy-set-display-transformer 'ivy-switch-buffer
+                               'ivy-rich-switch-buffer-transformer)
+  (ivy-rich-mode 1)) ;; this gets us descriptions in M-x.
+
+;; swiper
+(use-package swiper
+  :after ivy
+  :bind (("C-s" . swiper)))
+
 (use-package all-the-icons-dired
   :config
   (add-hook 'dired-mode-hook 'all-the-icons-dired-mode))
 
+;; makeing h,l do what the supposed to do
+(evil-define-key 'normal dired-mode-map
+(kbd "h") 'dired-up-directory
+(kbd "l") 'dired-find-file
+(kbd "SPC") 'nil) ; making keybindings start with SPC work in dired
+
 (setq delete-by-moving-to-trash t
       trash-directory "~/.local/share/Trash/files/")
+
+(set-face-attribute 'org-level-1 nil :height 1.8)
+(set-face-attribute 'org-level-2 nil :height 1.6)
+(set-face-attribute 'org-level-3 nil :height 1.4)
+(set-face-attribute 'org-level-4 nil :height 1.3)
+(set-face-attribute 'org-level-5 nil :height 1.2)
+(set-face-attribute 'org-level-6 nil :height 1.1)
+
+(use-package org-modern
+  :config (global-org-modern-mode 1))
+(setq org-hide-emphasis-markers t) ; hide markup signs like ~ ~ * * / / _ _
+
+(setq org-src-fontify-natively t
+    org-src-tab-acts-natively t
+    org-confirm-babel-evaluate nil
+    org-edit-src-content-indentation 0)
+
+(use-package org-tempo
+  :ensure nil) ; iguess it's installed but need to be activated
+
+(use-package toc-org
+  :commands toc-org-enable
+  :init (add-hook 'org-mode-hook 'toc-org-enable))
+
+(use-package org-auto-tangle
+ :defer t
+  :hook (org-mode . org-auto-tangle-mode))
+
+(use-package org-journal
+  :defer t
+  :init
+  ;; Change default prefix key; needs to be set before loading org-journal
+  (setq org-journal-prefix-key "C-c j ")
+  :config
+  (setq org-journal-dir "~/org/journal/"
+        org-journal-date-format "%A, %d %B %Y"
+        org-journal-file-format "%Y-%m-%d.org"))
+
+(nvmap :prefix "SPC"
+   "o a" '(org-agenda :which-key "opens org agenda")
+   "o w" '(org-agenda-list :which-key "agenda week view")
+   "o j" '(org-journal-new-entry :which-key "a new journal file")
+   "o c" '(org-journal-open-current-journal-file :which-key "open Current journal file"))
 
 (use-package magit
   :config
